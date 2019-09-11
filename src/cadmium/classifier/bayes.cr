8d300824b3542137bf09642c40261b06d3d37463
Chris Watson <cawatson1993@gmail.com>
2019-09-11 14:49:34 -0700

    Fix tokenizer

--- src/cadmium/classifier/bayes.cr
+++ src/cadmium/classifier/bayes.cr
@@ -43,7 +43,7 @@ module Cadmium
     include JSON::Serializable
     include YAML::Serializable
 
-    DEFAULT_TOKENIZER = Cadmium::Word.new
+    DEFAULT_TOKENIZER = Cadmium::Tokenizer::Word.new
 
     @[JSON::Field(ignore: true)]
     @[YAML::Field(ignore: true)]
